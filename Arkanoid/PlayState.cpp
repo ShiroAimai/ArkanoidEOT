cbfcd0e242ebeb8f58f450e5a242bbf1bc52067a
Nick <shiroaimai@gmail.com>
2021-12-08 18:15:50 +0100

    define victory and defeat

--- Arkanoid/PlayState.cpp
+++ Arkanoid/PlayState.cpp
@@ -3,10 +3,40 @@
 #include "Level1.h"
 #include "EndGameState.h"
 #include "EndGameLevel.h"
+#include "Brick.h"
 
-PlayState::PlayState(Game* GameInstance) : GameState(GameInstance), bIsGameOver(false)
+PlayState::PlayState(Game* GameInstance) : GameState(GameInstance), bIsGameOver(false), m_bricksCount(0)
 {
 	m_level = new Level1();
+}
+
+
+void PlayState::OnEnter()
+{
+	GameState::OnEnter();
+	std::vector<BaseObject*>& GameObjects = GetGameObjects();
+
+	for (BaseObject* object : GameObjects)
+	{
+		if (dynamic_cast<Brick*>(object) != nullptr)
+		{
+			++m_bricksCount;
+		}
+	}
+}
+
+void PlayState::RemoveGameObject(BaseObject* object, bool doDelete /*= true*/)
+{
+	//before removing object check if it is a brick, if it is decrement counter
+	if (dynamic_cast<Brick*>(object) != nullptr)
+	{
+		--m_bricksCount;
+		if (m_bricksCount == 0)
+		{
+			bIsGameOver = true; //player destroyed last brick, so he won
+		}
+	}
+	GameState::RemoveGameObject(object, doDelete);
 }
 
 void PlayState::Update(float deltaTime)
@@ -14,7 +44,7 @@ void PlayState::Update(float deltaTime)
 	GameState::Update(deltaTime);
 	if (bIsGameOver)
 	{
-		UpdateGameState(new EndGameState(true, game)); //todo update with game logic
+		UpdateGameState(new EndGameState(m_bricksCount == 0, game));
 	}
 }
 
--- Arkanoid/PlayState.h
+++ Arkanoid/PlayState.h
@@ -7,11 +7,14 @@ class PlayState : public GameState
 public:
 	PlayState(Game* GameInstance);
 	
+	virtual void OnEnter() override;
+	virtual void RemoveGameObject(BaseObject* object, bool doDelete = true);
 	virtual void Update(float deltaTime) override;
 
 	void GameOver();
 
 private:
+	int m_bricksCount;
 	bool bIsGameOver;
 };
 
